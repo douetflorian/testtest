53c4ef44e67123f5c66c237466697d7a87cfde2a
douetflorian <douetflo@gmail.com>
2017-03-03 15:21:06 +0100

    add projet

---
+++ TP bootstrap/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/font-awesome.css">
+	<title>PouetPouet Voyage</title>
+</head>
+<body>
+
+<?php 
+	include("menu.html"); 
+	include("carousel.html");
+?>
+
+
+<div class="container-fluid bg-3 text-center">    
+            <h2>Nos voyages</h2><br>
+            <div class="row portfolio">
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="imgFade">
+                        <img src="https://i2.wp.com/www.bestjobersblog.com/wp-content/uploads/2016/10/Niagara-01.jpg?resize=1080%2C721" class="img-responsive" style="width:100%" alt="Chute du Niagara">
+                        <div class="overlay">
+                            <div class="text">Chute du Niagara <br> 1600 &euro; <br><br> <div class="fa fa-arrow-right btn btn-default"> Réserver</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="imgFade">
+                        <img src="https://lonelyplanetimages.imgix.net/mastheads/16641625.jpg?sharp=10&vib=20&w=1200" class="img-responsive" style="width:100%" alt="Machu Picchu">
+                        <div class="overlay">
+                            <div class="text">Machu Picchu <br> 1200 &euro;<br><br> <div class="fa fa-arrow-right btn btn-default"> Réserver</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="imgFade">
+                        <img src="https://www.canyontours.com/wp-content/uploads/2013/09/West-Rim.jpg" class="img-responsive" style="width:100%" alt="Grand Canyon">
+                        <div class="overlay">
+                            <div class="text">Grand Canyon <br> 1000 &euro;<br><br> <div class="fa fa-arrow-right btn btn-default"> Réserver</div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row portfolio">
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="imgFade">
+                        <img src="https://i2.wp.com/www.bestjobersblog.com/wp-content/uploads/2016/10/Niagara-01.jpg?resize=1080%2C721" class="img-responsive" style="width:100%" alt="Chute du Niagara">
+                        <div class="overlay">
+                            <div class="text">Chute du Niagara <br> 1600 &euro;<br><br> <div class="fa fa-arrow-right btn btn-default"> Réserver</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="imgFade">
+                        <img src="https://lonelyplanetimages.imgix.net/mastheads/16641625.jpg?sharp=10&vib=20&w=1200" class="img-responsive" style="width:100%" alt="Machu Picchu">
+                        <div class="overlay">
+                            <div class="text">Machu Picchu <br> 1200 &euro;<br><br> <div class="fa fa-arrow-right btn btn-default"> Réserver</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="imgFade">
+                        <img src="https://www.canyontours.com/wp-content/uploads/2013/09/West-Rim.jpg" class="img-responsive" style="width:100%" alt="Grand Canyon">
+                        <div class="overlay">
+                            <div class="text">Grand Canyon <br> 1000 &euro;<br><br> <div class="fa fa-arrow-right btn btn-default"> Réserver</div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+<?php 
+	include("navigation.html");
+	include("footer.html"); 
+?>
+
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
